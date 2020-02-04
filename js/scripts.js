bfb687b30d917210d9e318fe0b90af06e81ca266
nick braeckman <nick.braeckman@gmail.com>
2020-02-04 20:56:30 +0100

    Adding initial files

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $('#profile-button').click(function(){
+    $('html,body').animate({
+      scrollTop: $('#about-section').offset().top
+    },1500);
+  });
+});
+
+$(document).ready(function(){
+  $('#profile-button').click(function(){
+    $('html,body').animate({
+      scrollTop: $('#about-section').offset().top
+    },1500);
+  });
+});
+
